ef8180ac08cfa32d2fed458f23dbfffcf7f7dc6a
randallhedglin <randallhedglin@yahoo.com>
2023-08-29 17:48:00 -0400

    Bootcamp step 7

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,10 +2,11 @@
 
 namespace App\Providers;
 
+use App\Events\ChirpCreated;
+use App\Listeners\SendChirpCreatedNotifications;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Event;
 
 class EventServiceProvider extends ServiceProvider
 {
@@ -15,6 +16,10 @@ class EventServiceProvider extends ServiceProvider
      * @var array<class-string, array<int, class-string>>
      */
     protected $listen = [
+        ChirpCreated::class => [
+            SendChirpCreatedNotifications::class,
+        ],
+
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
